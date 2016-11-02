1005ed85da6db7594d8a3549a79d11914febad6f
Sachin Joshi <software.wikipedia@gmail.com>
2016-11-02 16:17:00 -0700

    moved out the select on focus to external file

--- javascripts/titleVariation.js
+++ javascripts/titleVariation.js
@@ -1,19 +1,5 @@
 var debug = false;
 
-$.fn.selectOnFocus = function() {
-  var $this = $(this);
-  $this.focus(function() {
-    $this.select();
-
-    // Work around Chrome's little problem
-    $this.mouseup(function() {
-      // Prevent further mouseup intervention
-      $this.unbind("mouseup");
-      return false;
-    });
-  });
-};
-
 var log = function(msg) {
   if (debug) {
     console.log(msg);

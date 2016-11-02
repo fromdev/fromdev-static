4615d54822d2d4cf385b53cbe31f96115e55f041
Sachin Joshi <software.wikipedia@gmail.com>
2016-11-02 16:23:04 -0700

    Move templates to outside code for customization

--- javascripts/titleVariation.js
+++ javascripts/titleVariation.js
@@ -45,61 +45,24 @@ var showArray = function(arr) {
 };
 
 
-
-var templates = [
-  "Best TOPIC Apps for TARGET",
-  "Best TOPIC App for TARGET",
-  "Top TOPIC App for TARGET",
-  "Top TOPIC App for TARGET",
-  "Recommended TOPIC Apps for TARGET",
-  "Recommended TOPIC App for TARGET",
-  "Best Apps To Learn TOPIC for TARGET",
-  "Good TOPIC Apps for TARGET",
-  "Good TOPIC App for TARGET",
-  "Learn TOPIC Apps for TARGET",
-  "Learn TOPIC App for TARGET",
-  "TOPIC Apps for TARGET",
-  "TOPIC App for TARGET",
-  "Best TARGET TOPIC App",
-  "TOPIC for TARGET App",
-  "TARGET TOPIC Apps"
-];
-
 var getRandomInt = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-var generateTitles = function(targets, topics) {
-  var titleArray = new Array();
-  var currentTitle;
-  for (var template in templates) {
-    currentTitle = (' ' + templates[template]).slice(1);
-    for (var target in targets) {
-      if (targets[target]) {
-        currentTitle = currentTitle.replace("TARGET", targets[target]);
-        for (var topic in topics) {
-          if (topics[topic]) {
-            currentTitle = currentTitle.replace("TOPIC", topics[topic]);
-            titleArray.push(currentTitle);
-          }
-        }
-      }
-    }
-  }
-  return titleArray;
-};
+
 var generateTitlesRandom = function(targets, topics) {
   var titleArray = new Array();
-  var step;
-  var currentTitle;
-  for (step = 0; step < 75; step++) {
-    currentTitle = (' ' + templates[getRandomInt(0, templates.length - 1)]).slice(1);
-    currentTitle = currentTitle.replace("TARGET", targets[getRandomInt(0, targets.length - 1)]);
-    currentTitle = currentTitle.replace("TOPIC", topics[getRandomInt(0, topics.length - 1)]);
-    titleArray.push(currentTitle);
-    log(currentTitle);
+  if(templates) {
+    var step;
+    var currentTitle;
+    for (step = 0; step < 75; step++) {
+      currentTitle = (' ' + templates[getRandomInt(0, templates.length - 1)]).slice(1);
+      currentTitle = currentTitle.replace("TARGET", targets[getRandomInt(0, targets.length - 1)]);
+      currentTitle = currentTitle.replace("TOPIC", topics[getRandomInt(0, topics.length - 1)]);
+      titleArray.push(currentTitle);
+      log(currentTitle);
+    }
   }
-
   return titleArray;
 };
 

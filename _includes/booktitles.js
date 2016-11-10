0f93e0689f6ce4d61131aa0765b85d4195a0fc15
Sachin Joshi <software.wikipedia@gmail.com>
2016-11-09 18:02:59 -0800

    Create booktitles.js

---
+++ _includes/booktitles.js
@@ -0,0 +1,18 @@
+var templates = [
+  "Best TOPIC Books for TARGET",
+  "Best TOPIC Book for TARGET",
+  "Top TOPIC Book for TARGET",
+  "Top TOPIC Book for TARGET",
+  "Recommended TOPIC Books for TARGET",
+  "Recommended TOPIC Book for TARGET",
+  "Best Books To Learn TOPIC for TARGET",
+  "Good TOPIC Books for TARGET",
+  "Good TOPIC Book for TARGET",
+  "Learn TOPIC Books for TARGET",
+  "Learn TOPIC Book for TARGET",
+  "TOPIC Books for TARGET",
+  "TOPIC Book for TARGET",
+  "Best TARGET TOPIC Book",
+  "TOPIC for TARGET Book",
+  "TARGET TOPIC Books"
+];

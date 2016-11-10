62760aa0adafb1f4773fff65eec31852c6b5c2ab
Sachin Joshi <software.wikipedia@gmail.com>
2016-11-09 17:59:04 -0800

    Create apptitles.js

---
+++ _includes/apptitles.js
@@ -0,0 +1,18 @@
+var templates = [
+  "Best TOPIC Apps for TARGET",
+  "Best TOPIC App for TARGET",
+  "Top TOPIC App for TARGET",
+  "Top TOPIC App for TARGET",
+  "Recommended TOPIC Apps for TARGET",
+  "Recommended TOPIC App for TARGET",
+  "Best Apps To Learn TOPIC for TARGET",
+  "Good TOPIC Apps for TARGET",
+  "Good TOPIC App for TARGET",
+  "Learn TOPIC Apps for TARGET",
+  "Learn TOPIC App for TARGET",
+  "TOPIC Apps for TARGET",
+  "TOPIC App for TARGET",
+  "Best TARGET TOPIC App",
+  "TOPIC for TARGET App",
+  "TARGET TOPIC Apps"
+];

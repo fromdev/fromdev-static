5dc7b041a50ef9bf49d44183bd4158cb8f08616a
Sachin Joshi <software.wikipedia@gmail.com>
2017-02-05 08:35:25 -0800

    Update add-to-list-reply-templates.js

--- javascripts/add-to-list-reply-templates.js
+++ javascripts/add-to-list-reply-templates.js
@@ -2,7 +2,8 @@ var templates = [
 "Nice Tutorial - should add it to this HASHTAGS list TARGETURL TARGETUSER",
 "Awesome Tutorial - I recommend it to be listed on HASHTAGS list TARGETURL TARGETUSER",
 "TARGETUSER #Recommendation for a new useful page from @ on HASHTAGS list TARGETURL",
-"Hey TARGETUSER look at this link - good page to be added to your HASHTAGS list on TARGETURL"
+"Hey TARGETUSER look at this link - good page to be added to your HASHTAGS list on TARGETURL",
+"Good Tutorial TARGETUSER can include it in HASHTAGS list at TARGETURL"
 
 
 ];
